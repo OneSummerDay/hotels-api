162bd6625ba3aab36e3835398d3874cb38abb1e0
OneSummerDay <artem.kraievyi@gmail.com>
2026-01-11 17:54:43 +0200

    refactor: move hotels search params to schema

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import Depends, FastAPI, Query
 from typing import Optional
 from datetime import date
 
@@ -7,35 +7,32 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class HotelsSearchArgs():
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+        has_spa: Optional[bool] = None,
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.stars = stars
+        self.has_spa = has_spa
+
 class SHotel(BaseModel):
     name: str
     location: str
     stars: int
     has_spa: bool
 
-@app.get("/hotels", response_model=list[SHotel])
+@app.get("/hotels")
 def get_hotels(
-    location: str,
-    date_from: date, 
-    date_to: date,
-    stars: Optional[int] = Query(None, ge=1, le=5),
-    has_spa: Optional[bool] = None,
-):
-    hotels = [
-        {
-            "name": "Hotel Sunshine",
-            "location": "Beach City",
-            "stars": 4,
-            "has_spa": True
-        },
-        {
-            "name": "Mountain Retreat",
-            "location": "Hill Town",
-            "stars": 5,
-            "has_spa": False
-        }
-    ]
-    return  hotels
+    search_args: HotelsSearchArgs = Depends()
+) -> list[SHotel]:
+    return  search_args
 
 
 class SBooking(BaseModel):
