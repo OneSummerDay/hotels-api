a403cb5f5f1eafa37f6591bf3bbea8c6ca88b8ad
OneSummerDay <artem.kraievyi@gmail.com>
2026-01-11 15:01:10 +0200

    feat: validate query params and enforce stars constraints

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from typing import Optional
+from datetime import date
 
 app = FastAPI()
 
 @app.get("/hotels")
 def get_hotels(
-    location,
-    date_from, 
-    date_to,
-    stars: Optional[int] = None,
+    location: str,
+    date_from: date, 
+    date_to: date,
+    stars: Optional[int] = Query(None, ge=1, le=5),
     has_spa: Optional[bool] = None,
 ):
     return  date_from, date_to
