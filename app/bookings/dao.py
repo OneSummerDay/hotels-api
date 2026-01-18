0cf1ac236b59ebf18085c888b72115b44157bad7
OneSummerDay <artem.kraievyi@gmail.com>
2026-01-18 12:37:22 +0200

    refactor: extract common DB logic into BaseDAO

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,15 +1,10 @@
+from app.dao.base import BaseDAO
 from app.database import async_session_maker
 from app.bookings.models import Booking
 from sqlalchemy import select
 
 
 
-class BookingDAO:
+class BookingDAO(BaseDAO):
+    model = Booking
     
-    @classmethod
-    async def find_all(cls):
-        async with async_session_maker() as session:
-            query = select(Booking)
-            result = await session.execute(query)
-            bookings = result.fetchall()
-            return bookings
