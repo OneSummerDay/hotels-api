0e1633c22c7d8c85ea0a68c64a75fff6a5c80ad2
OneSummerDay <artem.kraievyi@gmail.com>
2026-01-18 14:23:37 +0200

    feat: add Booking schema for data validation

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.bookings.dao import BookingDAO
+from app.bookings.schemas import SBooking
 
 router = APIRouter(
     prefix="/bookings", 
@@ -8,6 +9,6 @@ router = APIRouter(
 )
 
 @router.get("/")
-async def get_bookings():
+async def get_bookings() -> list[SBooking]:
     return await BookingDAO.find_all()
     
