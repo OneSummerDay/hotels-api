ea09f54fb455b338696a1aa9be110210aa198b29
OneSummerDay <artem.kraievyi@gmail.com>
2026-01-18 12:57:41 +0200

    feat: implement get_one_or_none in BaseDAO

--- app/dao/base.py
+++ app/dao/base.py
@@ -7,6 +7,13 @@ class BaseDAO:
     model = None
 
     @classmethod
+    async def find_one_or_none(cls, **filter):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter(**filter)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+    @classmethod
     async def find_all(cls, **filter):
         async with async_session_maker() as session:
             query = select(cls.model).filter(**filter) 
