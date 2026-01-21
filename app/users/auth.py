7b7c9f68b47f2c0f9c7eea5dd4a6e2b1d978b3d0
OneSummerDay <artem.kraievyi@gmail.com>
2026-01-21 21:10:10 +0200

    feat: implement JWT access token generator

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,3 +1,4 @@
+import datetime
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -7,3 +8,10 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + datetime(minutes=15)
+    to_encode.update({"exp": expire})
+    encodet_jwt = jwt.encode(to_encode, "secret", algorithm="HS256")
+    return encodet_jwt
